b66b98a8202655b66f39a2206fd948b270660e71
erennkazan@gmail.com <erennkazan@gmail.com>
2023-08-01 23:41:54 +0300

    fixed

--- typescript-todolist/src/App.tsx
+++ typescript-todolist/src/App.tsx
@@ -1,13 +1,34 @@
-import React,{FC} from 'react';
+import React,{ChangeEvent, FC} from 'react';
 import './App.css';
-
+import { useState } from 'react';
 const App : FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [workDay, setWorkDay] = useState<number>(0);
+
+  const handleChange = (event:ChangeEvent<HTMLInputElement>) => {
+    if(event.target.name === "task"){
+      setTask(event.target.value);
+    }
+    else {
+      setWorkDay(Number(event.target.value));
+    }
+  };
   return (
     <div className="App">
       <div>
-        <input type="text" placeholder='Taskınızı Giriniz...' />
-        <input type="number" placeholder='Kaç Günde Tamamlamalısınız?' />
-
+        <input 
+        type="text" 
+        value={task} 
+        name="task"
+        placeholder='Taskınızı Giriniz...'
+        onChange={handleChange} />
+        <input 
+        type="number" 
+        value={workDay} 
+        name="workDay"
+        placeholder='Kaç Günde Tamamlamalısınız?' 
+        onChange={handleChange}/>
+        <button>Yeni Task Ekle</button>
       </div>
     </div>
   );
